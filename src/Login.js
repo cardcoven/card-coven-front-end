740ba164cceabf51b277dca2a2b3b69c6b49f8ac
davidabutler92 <davidabutler92@yahoo.com>
2020-11-16 15:26:41 -0800

    add signup component that creates a user

--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 
+
 export default class Login extends Component {
     render() {
         return (
             <div>
-                Login Page
+                login page
             </div>
         )
     }
--- src/SignUp.js
+++ src/SignUp.js
@@ -1,10 +1,48 @@
 import React, { Component } from 'react'
+import { createUser } from './mtgApi';
 
 export default class SignUp extends Component {
+
+    state = { 
+        email: '',
+        password: '',
+        loading: false
+    }
+
+    handleSubmit = async (e) => {
+        e.preventDefault();
+        this.setState({ loaidng: true });
+        const user = await createUser(this.state);
+        this.setState({ loading: false })
+        this.props.handleTokenUserChange(user.body.token, user.body.email)
+    }
+
     render() {
+        //return gif if loading is true
+        if(this.state.loading) return (
+            <img className='loader' alt='loader gif' src='https://www.cbc.ca/sports/longform/content/ajax-loader.gif'/>
+        )
         return (
             <div>
-                Signup page
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        <input
+                            required
+                            onChange={(e) => this.setState({ email: e.target.value })}
+                            type='text'
+                            placeholder='Email'
+                        ></input>
+                    </label>
+                    <label>
+                        <input
+                            required
+                            onChange={(e) => this.setState({ password: e.target.value })}
+                            type='password'
+                            placeholder='Password'
+                        ></input>
+                    </label>
+                    <button>Sign up</button>
+                </form>
             </div>
         )
     }
