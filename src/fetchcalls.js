0e33b8f09c0b1cbaac52421ea7f5cc5fe8ca84b2
JustinMartin7x <justin.martin7x@gmail.com>
2020-11-17 09:22:19 -0800

    messing with Api call

--- src/fetchcalls.js
+++ src/fetchcalls.js
@@ -1,9 +1,11 @@
 import request from 'superagent'
+const page = 'Page-Size'
 
 export async function fetchApi() {
     try {
         return await request
-            .get('https://api.magicthegathering.io/v1/cards')
+            .get('http://api.magicthegathering.io/v1/cards?pageSize=20')
+
     } catch (e) {
         return {
             error: e.message
