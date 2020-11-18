33ba0fc80679569a024303921858d1a2b45ed758
JustinMartin7x <justin.martin7x@gmail.com>
2020-11-18 14:35:08 -0800

    styled right border

--- src/RightDrawer.js
+++ src/RightDrawer.js
@@ -6,10 +6,17 @@ export default class RightDrawer extends Component {
     render() {
         return (
             <div className='right-drawer-div'>
-                <img className='right-drawer-img' src={this.props.card.imageUrl} alt={this.props.card.name}/>
-                Colors: {this.props.card.colors}
-                <p>Type: {this.props.card.type}</p>
-                <p className='right-drawer-card-text'>Card description: {this.props.card.text}</p>
+                <div className="display-image">
+                    <img className='right-drawer-img'
+                        src={this.props.card.imageUrl}
+                        alt={this.props.card.name}
+                    />
+                </div>
+                <div className="description-div">
+                    <p>Colors: {this.props.card.colors}</p>
+                    <p>Type: {this.props.card.type}</p>
+                    <p className='right-drawer-card-text'>Card description: {this.props.card.text}</p>
+                </div>
             </div>
         )
     }
