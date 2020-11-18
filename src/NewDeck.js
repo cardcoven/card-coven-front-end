7f124cd293227f968725c88e1922dbc54c08cf28
Jonathan Quesada <jonnythan.quesada@gmail.com>
2020-11-18 12:12:03 -0800

    added newDeck route and merged login into SignUp.js

---
+++ src/NewDeck.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react'
+import { createDeck } from './fetches/fetches';
+import './styles/newDeck.css';
+
+export default class NewDeck extends Component {
+    
+    state = {
+        
+    }
+
+    handleSubmit = async (e) => {
+
+        e.preventDefault();
+
+        await createDeck(
+            {
+            new_deck: this.state.new_deck,
+            new_description: this.state.new_description,
+            deck_type: true,
+            }, 
+            this.props.token
+        )
+
+        this.props.history.push('/list');
+
+    }
+    render() {
+        return (
+            <div className='new-deck-div'>
+                <form 
+                className='new-deck-form'
+                onSubmit={this.handleSubmit}>
+                    <label>New Deck Name<br></br>
+                        <input
+                            required
+                            onChange={(e) => this.setState({ new_deck: e.target.value })}
+                            type='text'
+                        ></input>
+                    </label>
+                    <label>Deck Description<br></br>
+                        <input
+                            required
+                            onChange={(e) => this.setState({ new_description: e.target.value })}
+                            type='text'
+                        ></input>
+                    </label>
+                    {
+                        this.state.loading 
+                        ? <img className='loader' alt='loader gif' src='https://www.cbc.ca/sports/longform/content/ajax-loader.gif'/>
+                        : <button>
+                            Submit
+                        </button>
+                    }
+                </form>
+            </div>
+        )
+    }
+}
