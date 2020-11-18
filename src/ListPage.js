61e0e52a3cddb188a01280330a1938c75f19db1c
JustinMartin7x <justin.martin7x@gmail.com>
2020-11-17 16:42:50 -0800

    left side

--- src/ListPage.js
+++ src/ListPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { fetchAllCards } from './mtgApi';
+import { fetchAllCards, fetchByType, fetchBySubType, fetchByMana } from './mtgApi';
 import './styles/list.css';
 import LeftDrawer from './LeftDrawer';
 import RightDrawer from './RightDrawer';
@@ -24,13 +24,13 @@ export default class ListPage extends Component {
         })
         const results = await fetchAllCards(this.state.page)
         console.log(results)
-        console.log(results);
+        console.log(this.state.page)
         this.setState({
             cards: results.body.cards,
             loading: false,
         })
     }
-    
+
     handleNextPage = async () => {
         this.setState({ page: this.state.page + 1 })
         this.fetchAll(this.state.page);
@@ -39,35 +39,74 @@ export default class ListPage extends Component {
     handlePrevPage = async () => {
         this.setState({ page: this.state.page - 1 })
         this.fetchAll(this.state.page);
-        }
+    }
+    handleTypeChange = async (e) => {
+        this.setState({
+            loading: true
+        })
+        const results = await fetchByType(this.state.page, e.target.value)
+        console.log(e.target.value)
+        this.setState({
+            cards: results.body.cards,
+            loading: false
+        })
+    }
+    handleSubTypeChange = async (e) => {
+        this.setState({
+            loading: true
+        })
+        const results = await fetchBySubType(this.state.page, e.target.value)
+        console.log(e.target.value)
+        this.setState({
+            cards: results.body.cards,
+            loading: false
+        })
+    }
+    handleManaChange = async (e) => {
+        this.setState({
+            loading: true
+        })
+        const results = await fetchByMana(this.state.page, e.target.value)
+        console.log(e.target.value)
+        this.setState({
+            cards: results.body.cards,
+            loading: false
+        })
+    }
+
 
     render() {
         return (
             <>
                 <div className='main-list-div'>
-                    <LeftDrawer />
+                    <LeftDrawer
+                        handleTypeChange={this.handleTypeChange}
+                        handleSubTypeChange={this.handleSubTypeChange}
+                        handleManaChange={this.handleManaChange} />
                     <div className='card-container'>
                         {
-                        this.state.cards.length ?
-                            this.state.cards
-                                .filter(item => item.imageUrl)
-                                .map(card =>
-                                    <div
-                                        key={card.id}
-                                        className='image-div'>
-                                        <img src={card.imageUrl || ''}
-                                            onError={i => i.target.src = ''}
-                                            alt={card.name} />
-                                    </div>)
-                            : <img className='loader' alt='loader gif' src='https://www.cbc.ca/sports/longform/content/ajax-loader.gif' />
+                            this.state.cards.length ?
+                                this.state.cards
+                                    .filter(item => item.imageUrl)
+                                    .map(card =>
+                                        <div
+                                            key={card.id}
+                                            className='image-div'>
+                                            <img src={card.imageUrl || ''}
+                                                onError={i => i.target.src = ''}
+                                                alt={card.name} />
+                                        </div>)
+                                : <img className='loader' alt='loader gif' src='https://www.cbc.ca/sports/longform/content/ajax-loader.gif' />
 
                         }
-                    <PagingButton 
-                    handlePaging={{ next: this.handleNextPage, 
-                    prev: this.handlePrevPage }}
-                    count={this.state.count}
-                    page={this.state.page}
-                    />
+                        <PagingButton
+                            handlePaging={{
+                                next: this.handleNextPage,
+                                prev: this.handlePrevPage
+                            }}
+                            count={this.state.count}
+                            page={this.state.page}
+                        />
                     </div>
                     <RightDrawer />
                 </div>
