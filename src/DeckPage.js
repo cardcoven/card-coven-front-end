17b73fb4f888413cba07569c3785ab52ef5b8bc9
DonnyLVu <donnyvu897@gmail.com>
2020-11-19 09:27:34 -0800

    add better spinners

--- src/DeckPage.js
+++ src/DeckPage.js
@@ -28,51 +28,57 @@ export default class DeckPage extends Component {
         console.log(response.body)
     }
 
-       render() {
+    render() {
         return (
             <div className='deck-page-card-container'>
                 <div className='deck-page-left'>
-                            {
-                                !!this.state.decks.length ?
-                                    this.state.decks
-                                        .map(deck =>
-                                            <>
-                                            <div
-                                                className='deck-page-div'
-                                                key={deck.id + deck.deck_name}
-                                                >
+                    {
+                        !!this.state.decks.length ?
+                            this.state.decks
+                                .map(deck =>
+                                    <>
+                                        <div
+                                            className='deck-page-div'
+                                            key={deck.id + deck.deck_name}
+                                        >
                                             <div className='deck-page-button'>
-                                                <button 
-                                                onClick={this.handleClick}
-                                                value={deck.id}
+                                                <button
+                                                    onClick={this.handleClick}
+                                                    value={deck.id}
                                                 >List</button>
                                             </div>
                                             <div>Deck Name: {deck.deck_name}<br></br>Deck Description: {deck.deck_description}</div>
-                                            </div>
-                                            </>
-                                            )
-                                    : <img className='loader' alt='loader gif' src='https://www.cbc.ca/sports/longform/content/ajax-loader.gif' />
+                                        </div>
+                                    </>
+                                )
+                            : <img className='loader' alt='loader gif' src='https://media.giphy.com/media/13hzdQ3QCID172/giphy.gif' />
 
-                            }
+                    }
                 </div>
 
                 <div className='deck-page-right'>
-                    
+
                     {
                         !!this.state.cards.length ?
-                        this.state.cards
-                            .map(card =>
-                                <img 
-                                alt={card.name} 
-                                src={card.img_url} 
-                                key={card.name + card.id + Math.random()}
-                                className='deck-page-card-img'
-                                />
+                            this.state.cards
+                                .map(card =>
+                                    <img
+                                        alt={card.name}
+                                        src={card.img_url}
+                                        key={card.name + card.id + Math.random()}
+                                        className='deck-page-card-img'
+                                    />
                                 )
-                        : <img className='loader' alt='loader gif' src='https://www.cbc.ca/sports/longform/content/ajax-loader.gif' />
+                            : <img className='loader' alt='loader gif' height='500px' width='auto' src='https://media.giphy.com/media/13hzdQ3QCID172/giphy.gif' />
                     }
                 </div>
             </div>
         )
     }
 }
+
+// https://giphy.com/gifs/cat-catch-mtg-13hzdQ3QCID172
+
+// For all cards https://giphy.com/gifs/wizardsmagic-mtg-arena-cat-1QdNRW77hbA4lGhdRl/links https://media.giphy.com/media/1QdNRW77hbA4lGhdRl/giphy.gif
+
+// For other type ones https://giphy.com/gifs/reddit-doing-lJNoBCvQYp7nq
